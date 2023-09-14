5b3e4d12235086a7bd157611ae4bbaadcb4d70fb
dilushaeka <dilushaeka99@gmail.com>
2023-09-15 02:04:41 +0530

    Created Web App Configuration class

---
+++ src/main/java/lk/ijse/gde/springmvcassi1/config/WebAppConfig.java
@@ -0,0 +1,22 @@
+package lk.ijse.gde.springmvcassi1.config;
+
+/*This Application Is Copyright Protected
+    Author : Dilusha Ekanayaka
+    Email  : dilushaeka99@gmail.com
+    Date   : 9/15/2023
+    Time   : 2:01 AM 
+*/
+
+import lk.ijse.gde.springmvcassi1.WebAppInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackageClasses = WebAppInitializer.class)
+public class WebAppConfig {
+    public WebAppConfig(){
+        System.out.println("Spring is Working");
+    }
+}
